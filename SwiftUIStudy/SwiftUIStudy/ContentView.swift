b610bfbbe56a86d61dcf863262ecc3a972cde821
huangcr <huangcr@baihe.com>
2020-06-22 10:14:23 +0800

    Furst Commit

---
+++ SwiftUIStudy/SwiftUIStudy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIStudy
+//
+//  Created by 黄成瑞 on 2020/6/22.
+//  Copyright © 2020 xiaoxiao. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
